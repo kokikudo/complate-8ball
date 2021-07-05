a31c6c8a78240e897b975e0862b65bb0e88c3ea4
kokikudo <massanmesu@icloud.com>
2021-07-05 22:01:29 +0900

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  //const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Center(child: Ball()),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  //const Ball({Key? key}) : super(key: key);
+
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int _ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        setState(() {
+          _ballNumber = Random().nextInt(5) + 1;
+        });
+      },
+      child: Image.asset('images/ball$_ballNumber.png'),
+    );
+  }
+}
